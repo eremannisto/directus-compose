15da9533958acbeb9fc6727e7ae67cd9bfbb17d0
Ere Männistö <connect@eremannisto.com>
2024-07-21 20:26:18 +0300

    Delete set-permissions.sh

--- sqlite/scripts/set-permissions.sh
+++
@@ -1,2 +0,0 @@
-chown -R 1000:1000 /directus/uploads /directus/extensions /directus/database /directus/templates
-chmod -R 755 /directus/uploads /directus/extensions /directus/database /directus/templates
